e999c0a6ae7f7d563145a772fae221d3aef3fe44
trcosner <trcosner@gmail.com>
2025-06-19 20:54:48 -0400

    Updates

--- src/middlewares/current-user.ts
+++ src/middlewares/current-user.ts
@@ -1,9 +1,11 @@
 import { Request, Response, NextFunction } from "express";
 import jwt from "jsonwebtoken";
+import { redisClient } from "../redis/redis-client";
 
 interface UserPayload {
   id: string;
   email: string;
+  jti: string;
 }
 
 declare global {
@@ -14,7 +16,7 @@ declare global {
   }
 }
 
-export const currentUser = (
+export const currentUser = async (
   req: Request,
   res: Response,
   next: NextFunction
@@ -28,6 +30,14 @@ export const currentUser = (
       req.session.jwt,
       process.env.JWT_KEY!
     ) as UserPayload;
+
+    const isBlacklisted = await redisClient.get(`blacklist:${payload.jti}`);
+    if (isBlacklisted === "1") {
+      console.log(`Token ${payload.jti} is blacklisted`);
+      req.session = null;
+      return next();
+    }
+
     req.currentUser = payload;
   } catch (err) {}
 
